bd1465b9d5d8facc82adb1706b8ca48cf3ad1121
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2021-08-08 16:42:23 -0700

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <a href="/auth">link to auth</a>
+      <Link href="/auth">link to auth</Link>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
